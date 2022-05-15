154ecb3c57cb1dbcc10d0ca35424d056ffc81b02
radupadurariu <padurariuionutradu@gmail.com>
2022-05-15 16:54:10 +0300

    redux initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,28 @@ import Navbar from '../src/components/layout/Navbar';
 import Landing from '../src/components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-	<Router>
-		<div className="App">
-			<Navbar />
-			<div className='content'>
-				
-				<Route exact path="/" component={Landing} />
-				<Switch>
-					<Route exact path="/register" component={Register} />
-					<Route exact path="/login" component={Login} />
-				</Switch>
+	<Provider store={store}>
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div className='content'>
+					
+					<Route exact path="/" component={Landing} />
+					<Switch>
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+					</Switch>
+					
+				</div>
+				<h3 className='footer'>@ copywrite 2022 - Radu Padurariu</h3>
 				
 			</div>
-			<h3 className='footer'>@ copywrite 2022 - Radu Padurariu</h3>
-			
-		</div>
-	</Router>
+		</Router>
+	</Provider>
 );
 
 export default App;
