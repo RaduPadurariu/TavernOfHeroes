58784434735b78d300526ab7ab31a00aaac36db0
radupadurariu <padurariuionutradu@gmail.com>
2024-02-05 15:15:29 +0200

    update dependencies, fix errors, rename Mongo credentials

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import authRouter from './api/auth.js';
-import postsRouter from './api/posts.js';
-import usersRouter from './api/users.js';
-import profileRouter from './api/profile.js';
+import express from "express";
+import authRouter from "./api/auth.js";
+import postsRouter from "./api/posts.js";
+import usersRouter from "./api/users.js";
+import profileRouter from "./api/profile.js";
 
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import "dotenv/config";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -14,36 +14,32 @@ app.use(bodyParser.json());
 
 // routes
 // app.get('/', (req, res) => res.send('Hello Social3 Server!'));
-app.use('/api/auth', authRouter);
-app.use('/api/posts', postsRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/profile', profileRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/profile", profileRouter);
 
 // heroku special env
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
-	// npm run build for react app
-	app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  // npm run build for react app
+  app.use(express.static("client/build"));
 
-	app.get('*', (req, res) => {
-		req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-	});
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 
 // Connect to DataBase
-const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
 
-		console.log('MongoDB connected!!');
-	} catch (err) {
-		console.log('Failed to connect to MongoDB', err);
-	}
-};
-connectDB();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to MongoDB", err);
+  });
 
 // start the server - listen on port 3000
 app.listen(process.env.PORT || 5000);
